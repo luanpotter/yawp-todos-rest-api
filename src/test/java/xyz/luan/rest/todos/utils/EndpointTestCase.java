3da891e4c930e78ff376fdcb8e7ccc26af86b037
Luan Nico <luanpotter27@gmail.com>
2019-03-07 19:46:35 -0300

    yawp bootstrap

---
+++ src/test/java/xyz/luan/rest/todos/utils/EndpointTestCase.java
@@ -0,0 +1,12 @@
+package xyz.luan.rest.todos.utils;
+
+import io.yawp.testing.EndpointTestCaseBase;
+
+public class EndpointTestCase extends EndpointTestCaseBase {
+
+    @Override
+    protected String getAppPackage() {
+        return "xyz.luan.rest.todos";
+    }
+
+}
